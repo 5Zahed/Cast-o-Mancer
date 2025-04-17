1658825073faa605931ceedd5e2c14dba7dca074
5Zahed <zahedkakar5@gmail.com>
2025-04-17 20:58:49 +0530

    added api/frame

---
+++ src/app/api/frame/route.ts
@@ -0,0 +1,21 @@
+export const dynamic = "force-dynamic";
+
+export async function POST() {
+  const html = `
+    <!DOCTYPE html>
+    <html>
+      <head>
+        <meta property="fc:frame" content="vnext" />
+        <meta property="fc:frame:image" content="https://cast-o-mancer.vercel.app/backgroun.png" />
+        <meta property="fc:frame:button:1" content="Cast Again" />
+        <meta property="fc:frame:post_url" content="https://cast-o-mancer.vercel.app/api/frame" />
+      </head>
+      <body>cast updated.</body>
+    </html>
+  `;
+
+  return new Response(html, {
+    status: 200,
+    headers: { "Content-Type": "text/html" },
+  });
+}
